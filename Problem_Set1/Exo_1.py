207fb2b342d93988122ad37e6c512187448c74cc
buzingga <nicoboisseau12@gmail.fr>
2022-03-31 01:18:30 +0200

    exo1 done

--- Problem_Set1/Exo_1.py
+++ Problem_Set1/Exo_1.py
@@ -1,1 +1,13 @@
-print("Hello Word !");
+def swap(a,b,c):
+    temp = c
+    c = b
+    b = a
+    a = temp
+    return a,b,c
+
+print("Hello Word !")
+a,b,c = 1,2,3
+print(a,b,c,"\n")
+a,b,c = swap(a,b,c)
+print(a,b,c,"\n")
+
