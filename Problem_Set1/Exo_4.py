2f728661e60d57ad793627da85887b7d3c47942b
buzingga <nicoboisseau12@gmail.fr>
2022-04-01 11:29:36 +0200

    Correction Exo_4

--- Problem_Set1/Exo_4.py
+++ Problem_Set1/Exo_4.py
@@ -1,12 +1,8 @@
 # A
-init_stock_price = input("please enter a stock price: ")
-init_stock_price = int(init_stock_price)
+stock_price = input("please enter a stock price: ")
+stock_price = int(stock_price)
+temp = stock_price - 50
 #intrinsic_value = stock_price > 50 and stock_price - 50 or stock_price
 # B
-stock_price = init_stock_price
-first_intrinsic_value = stock_price > 50 and init_stock_price - 50 or stock_price
-print(first_intrinsic_value)
-second_intrinsic_value = stock_price >=100 and 0 or first_intrinsic_value
-print(second_intrinsic_value)
-third_intrinsic_value = stock_price > 50 and first_intrinsic_value or second_intrinsic_value
-print(third_intrinsic_value)
+intrinsic_value = ((stock_price,temp)[stock_price > 50],0)[stock_price >= 100]
+print(intrinsic_value)
